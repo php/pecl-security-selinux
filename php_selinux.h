dd80cb188a7db060e8b67a13c40436109248a18a
Remi Collet <remi@php.net>
2024-10-08 15:41:09 +0200

    Release 0.6.1

--- php_selinux.h
+++ php_selinux.h
@@ -4,7 +4,7 @@
 extern zend_module_entry		selinux_module_entry;
 #define phpext_selinux_ptr		&selinux_module_entry
 
-#define PHP_SELINUX_VERSION "0.6.1-dev"
+#define PHP_SELINUX_VERSION "0.6.1"
 #define PHP_SELINUX_STATE   "stable"
 
 #endif	/* PHP_SELINUX_H */
